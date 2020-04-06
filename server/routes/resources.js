83e3f26531e2661b5ad7fe3d561d2f985302b8d7
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2020-04-06 14:34:52 -0500

    format

--- server/routes/resources.js
+++ server/routes/resources.js
@@ -1,17 +1,15 @@
-import express from 'express'
-import axios from 'axios'
-import {writeFile} from 'fs'
+import express from "express";
+import axios from "axios";
+import { writeFile } from "fs";
 
 const router = express.Router();
 
-const URL = `https://sheets.googleapis.com/v4/spreadsheets/1B5TGoyQFAvqz0ZVnaJPGPbIFPiIvv4BoVr1flLnITRg/values/Clinical!1:198?key=AIzaSyCKy9qEhymMYkaW3m-92W8WIRbk5UPlP6w`
+const URL = `https://sheets.googleapis.com/v4/spreadsheets/1B5TGoyQFAvqz0ZVnaJPGPbIFPiIvv4BoVr1flLnITRg/values/Clinical!1:198?key=AIzaSyCKy9qEhymMYkaW3m-92W8WIRbk5UPlP6w`;
 /* GET resources listing. */
-router.get('/', async function(req, res, next) {
-  const resp = await axios.get(URL)
+router.get("/", async function (req, res, next) {
+  const resp = await axios.get(URL);
 
-  res.json({resources: resp.data.values})
+  res.json({ resources: resp.data.values });
 });
 
-export default router
-
-
+export default router;
